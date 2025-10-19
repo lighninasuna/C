b885036570396afd7b05424afa0d59fa000aa84b
lighninasuna <lekimnguyen16102007@gmail.com>
2025-10-19 15:03:45 +0200

    vaica

--- helo.c
+++ helo.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 int main()
 {
-<<<<<<< HEAD
-    printf("hello toi\n");
-=======
-    printf("hello ban \n");
->>>>>>> 3115a29fce8ec2da3a29ace0015464bce33de123
+    printf("hello toi nha\n");
     return 0;
 }
